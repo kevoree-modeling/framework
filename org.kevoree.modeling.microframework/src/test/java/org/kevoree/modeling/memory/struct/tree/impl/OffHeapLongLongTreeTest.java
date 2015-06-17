0454fdbf2e906c63d0655195faef5a866909ffc9
Thomas Hartmann <thomi.hartmann@gmail.com>
2015-06-17 12:01:32 +0200

    * offheap stuff

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/struct/tree/impl/OffHeapLongLongTreeTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/struct/tree/impl/OffHeapLongLongTreeTest.java
@@ -2,15 +2,11 @@ package org.kevoree.modeling.memory.struct.tree.impl;
 
 import org.kevoree.modeling.memory.struct.tree.BaseKLongLongTreeTest;
 import org.kevoree.modeling.memory.struct.tree.KLongLongTree;
-import org.kevoree.modeling.memory.struct.tree.KLongTree;
 
-/**
- * Created by duke on 01/12/14.
- */
-public class LongLongTreeTest extends BaseKLongLongTreeTest {
+public class OffHeapLongLongTreeTest extends BaseKLongLongTreeTest {
 
     @Override
     public KLongLongTree createKLongLongTree() {
-        return new ArrayLongLongTree();
+        return new OffHeapLongLongTree();
     }
 }
