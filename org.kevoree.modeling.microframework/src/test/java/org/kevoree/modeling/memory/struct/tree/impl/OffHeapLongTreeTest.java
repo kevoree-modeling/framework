8e3371329f8e5ef6a4d65e0a9f4896ef74c3af32
Grégory Nain <gregory.nain@gmail.com>
2015-06-17 11:28:19 +0200

    Ignore OffHeap tests generation for TS

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/struct/tree/impl/OffHeapLongTreeTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/struct/tree/impl/OffHeapLongTreeTest.java
@@ -3,6 +3,7 @@ package org.kevoree.modeling.memory.struct.tree.impl;
 import org.kevoree.modeling.memory.struct.tree.BaseKLongTreeTest;
 import org.kevoree.modeling.memory.struct.tree.KLongTree;
 
+/** @ignore ts*/
 public class OffHeapLongTreeTest extends BaseKLongTreeTest {
 
     @Override
