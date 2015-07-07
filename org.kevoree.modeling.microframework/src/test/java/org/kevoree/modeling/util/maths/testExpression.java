c4ec4d62427c9022e58e10c27edd860204d45ba8
Assaad Moawad <assaad.moawad@uni.lu>
2015-07-07 12:15:27 +0200

    Update test case

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/util/maths/testExpression.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/util/maths/testExpression.java
@@ -8,6 +8,6 @@ public class testExpression {
     public void expressionTest(){
         Expression ex = new Expression("(3.5+price*8-14/7)%4");
         ex.setVariable("price","10");
-        System.out.println(ex.eval());
+        Assert.assertEquals(ex.eval(),1.5,0.0001);
     }
 }
