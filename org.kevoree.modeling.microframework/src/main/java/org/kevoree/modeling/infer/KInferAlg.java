a38ff7a6f971956d0c0f6584930a8528faf22d80
Francois Fouquet <fouquet.f@gmail.com>
2015-07-08 10:22:09 +0200

    add depdendency param for KInferAlg API

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/infer/KInferAlg.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/infer/KInferAlg.java
@@ -1,11 +1,12 @@
 package org.kevoree.modeling.infer;
 
 import org.kevoree.modeling.KObject;
+import org.kevoree.modeling.meta.KMetaDependencies;
 
 public interface KInferAlg {
 
-    void train(double[][] trainingSet, double[] expectedResultSet, KObject origin);
+    void train(double[][] trainingSet, double[] expectedResultSet, KObject currentInferObject, KMetaDependencies meta);
 
-    double[] infer(double[] features, KObject origin);
+    double[] infer(double[] features, KObject origin, KMetaDependencies meta);
 
 }
--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/infer/impl/AvgInferAlg.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/infer/impl/AvgInferAlg.java
@@ -2,17 +2,19 @@ package org.kevoree.modeling.infer.impl;
 
 import org.kevoree.modeling.KObject;
 import org.kevoree.modeling.infer.KInferAlg;
+import org.kevoree.modeling.meta.KMetaDependencies;
 
 //TODO
 public class AvgInferAlg implements KInferAlg {
 
     @Override
-    public void train(double[][] trainingSet, double[] expectedResultSet, KObject origin) {
+    public void train(double[][] trainingSet, double[] expectedResultSet, KObject origin, KMetaDependencies meta) {
+
 
     }
 
     @Override
-    public double[] infer(double[] features, KObject origin) {
+    public double[] infer(double[] features, KObject origin, KMetaDependencies meta) {
         return null;
     }
 
