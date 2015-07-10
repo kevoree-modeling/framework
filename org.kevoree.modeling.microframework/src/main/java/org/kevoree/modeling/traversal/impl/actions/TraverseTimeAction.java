e1eb83fea655798a9d87305f6ae59e6c4412d67e
Francois Fouquet <fouquet.f@gmail.com>
2015-07-10 18:24:53 +0200

    add timeTraversalAction

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/traversal/impl/actions/TraverseTimeAction.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/traversal/impl/actions/TraverseTimeAction.java
@@ -2,13 +2,22 @@ package org.kevoree.modeling.traversal.impl.actions;
 
 import org.kevoree.modeling.traversal.KTraversalAction;
 import org.kevoree.modeling.traversal.KTraversalActionContext;
+import org.kevoree.modeling.traversal.KTraversalFilter;
 
 public class TraverseTimeAction implements KTraversalAction {
 
     private KTraversalAction _next;
 
-    public TraverseTimeAction() {
-        
+    private long _timeOffset;
+
+    private long _steps;
+
+    private KTraversalFilter _continueContition;
+
+    public TraverseTimeAction(long p_timeOffset, long p_steps, KTraversalFilter p_continueCondition) {
+        this._timeOffset = p_timeOffset;
+        this._steps = p_steps;
+        this._continueContition = p_continueCondition;
     }
 
     @Override
@@ -18,7 +27,7 @@ public class TraverseTimeAction implements KTraversalAction {
 
     @Override
     public void execute(KTraversalActionContext context) {
-
+        throw new RuntimeException("Not implemented Yet!");
     }
 
 }
