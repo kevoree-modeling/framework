410f48e60c9e0db4286851a93c5cb7e313c99e76
Thomas Hartmann <thomi.hartmann@gmail.com>
2015-07-30 16:56:29 +0200

    * improved storage tests

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/storage/BaseKMemoryStorageTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/storage/BaseKMemoryStorageTest.java
@@ -3,68 +3,70 @@ package org.kevoree.modeling.memory.storage;
 import org.junit.Assert;
 import org.junit.Test;
 import org.kevoree.modeling.KConfig;
-import org.kevoree.modeling.memory.KMemoryElement;
-import org.kevoree.modeling.meta.KMetaModel;
+import org.kevoree.modeling.memory.chunk.KMemoryChunk;
+import org.kevoree.modeling.memory.map.KUniverseOrderMap;
+import org.kevoree.modeling.memory.tree.KLongLongTree;
+import org.kevoree.modeling.memory.tree.KLongTree;
 
 public abstract class BaseKMemoryStorageTest {
 
     public abstract KMemoryStorage createKMemoryStorage();
 
+    public abstract KLongLongTree createKLongLongTree();
+
+    public abstract KLongTree createKLongTree();
+
+    public abstract KUniverseOrderMap createKUniverseOrderMap();
+
+    public abstract KMemoryChunk createKMemoryChunk();
+
     @Test
     public void test() {
-        KMemoryStorage cache = createKMemoryStorage();
+        KMemoryStorage storage = createKMemoryStorage();
 
-        KMemoryElement temp = new KMemoryElement() {
-            @Override
-            public boolean isDirty() {
-                return false;
-            }
+        // KUniverseOrderMap
+        KUniverseOrderMap map = createKUniverseOrderMap();
+        map.put(0, 0);
+        map.put(1, 1);
+        storage.putAndReplace(KConfig.NULL_LONG, KConfig.NULL_LONG, KConfig.NULL_LONG, map);
+        KUniverseOrderMap retrievedMap = (KUniverseOrderMap) storage.get(KConfig.NULL_LONG, KConfig.NULL_LONG, KConfig.NULL_LONG);
+        Assert.assertEquals(0, retrievedMap.get(0));
+        Assert.assertEquals(1, retrievedMap.get(1));
+        Assert.assertEquals(map.size(), retrievedMap.size());
+        Assert.assertEquals(map.isDirty(), retrievedMap.isDirty());
+        Assert.assertEquals(map.counter(), retrievedMap.counter());
 
-            @Override
-            public String serialize(KMetaModel metaModel) {
-                return null;
-            }
+        // KLongLongTree
+        KLongLongTree longLongTree = createKLongLongTree();
+        longLongTree.init(null, null);
+        longLongTree.insert(0, 0);
+        longLongTree.insert(1, 1);
+        storage.putAndReplace(0, KConfig.NULL_LONG, KConfig.END_OF_TIME, longLongTree);
+        KLongLongTree retrievedLongLongTree = (KLongLongTree) storage.get(0, KConfig.NULL_LONG, KConfig.END_OF_TIME);
+        Assert.assertEquals(0, retrievedLongLongTree.lookupValue(0));
+        Assert.assertEquals(1, retrievedLongLongTree.lookupValue(1));
+        Assert.assertEquals(longLongTree.size(), retrievedLongLongTree.size());
+        Assert.assertEquals(longLongTree.isDirty(), retrievedLongLongTree.isDirty());
+        Assert.assertEquals(longLongTree.counter(), retrievedLongLongTree.counter());
 
-            @Override
-            public void setClean(KMetaModel mm) {
-            }
+        // KLongTree
+        KLongTree longTree = createKLongTree();
+        longTree.init(null, null);
+        longTree.insert(0);
+        longTree.insert(1);
+        storage.putAndReplace(0, KConfig.NULL_LONG, 0, longTree);
+        KLongTree retrievedLongTree = (KLongTree) storage.get(0, KConfig.NULL_LONG, 0);
+        Assert.assertEquals(0, retrievedLongTree.lookup(0));
+        Assert.assertEquals(1, retrievedLongTree.lookup(1));
+        Assert.assertEquals(longTree.size(), retrievedLongTree.size());
+        Assert.assertEquals(longTree.isDirty(), retrievedLongTree.isDirty());
+        Assert.assertEquals(longTree.counter(), retrievedLongTree.counter());
 
-            @Override
-            public void setDirty() {
-
-            }
-
-            @Override
-            public void init(String payload, KMetaModel metaModel) {
-            }
-
-            @Override
-            public int counter() {
-                return 0;
-            }
-
-            @Override
-            public void inc() {
-            }
-
-            @Override
-            public void dec() {
-            }
-
-            @Override
-            public void free(KMetaModel metaModel) {
-
-            }
-        };
-        cache.putAndReplace(KConfig.NULL_LONG, KConfig.NULL_LONG, KConfig.NULL_LONG, temp);
-        Assert.assertEquals(temp, cache.get(KConfig.NULL_LONG, KConfig.NULL_LONG, KConfig.NULL_LONG));
-        cache.putAndReplace(0, KConfig.NULL_LONG, KConfig.NULL_LONG, temp);
-        Assert.assertEquals(temp, cache.get(KConfig.NULL_LONG, KConfig.NULL_LONG, KConfig.NULL_LONG));
-        Assert.assertEquals(temp, cache.get(0, KConfig.NULL_LONG, KConfig.NULL_LONG));
-        cache.putAndReplace(KConfig.NULL_LONG, KConfig.NULL_LONG, 2, temp);
-        Assert.assertEquals(temp, cache.get(KConfig.NULL_LONG, KConfig.NULL_LONG, 2));
-
-
+        // KMemoryChunk
+        KMemoryChunk chunk = createKMemoryChunk();
+        storage.putAndReplace(0, 0, 0, chunk);
+        KMemoryChunk retrievedChunk = (KMemoryChunk) storage.get(0, 0, 0);
+        Assert.assertNotNull(retrievedChunk);
     }
 }
 
--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/storage/impl/ArrayMemoryStorageTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/storage/impl/ArrayMemoryStorageTest.java
@@ -1,12 +1,41 @@
 package org.kevoree.modeling.memory.storage.impl;
 
+import org.kevoree.modeling.KConfig;
+import org.kevoree.modeling.memory.chunk.KMemoryChunk;
+import org.kevoree.modeling.memory.chunk.impl.HeapMemoryChunk;
+import org.kevoree.modeling.memory.map.KUniverseOrderMap;
+import org.kevoree.modeling.memory.map.impl.ArrayUniverseOrderMap;
 import org.kevoree.modeling.memory.storage.BaseKMemoryStorageTest;
 import org.kevoree.modeling.memory.storage.KMemoryStorage;
+import org.kevoree.modeling.memory.tree.KLongLongTree;
+import org.kevoree.modeling.memory.tree.KLongTree;
+import org.kevoree.modeling.memory.tree.impl.ArrayLongLongTree;
+import org.kevoree.modeling.memory.tree.impl.ArrayLongTree;
 
 public class ArrayMemoryStorageTest extends BaseKMemoryStorageTest {
 
     @Override
     public KMemoryStorage createKMemoryStorage() {
         return new ArrayMemoryMemoryStorage(null);
+    }
+
+    @Override
+    public KLongLongTree createKLongLongTree() {
+        return new ArrayLongLongTree();
+    }
+
+    @Override
+    public KLongTree createKLongTree() {
+        return new ArrayLongTree();
+    }
+
+    @Override
+    public KUniverseOrderMap createKUniverseOrderMap() {
+        return new ArrayUniverseOrderMap(10, KConfig.CACHE_LOAD_FACTOR, null);
+    }
+
+    @Override
+    public KMemoryChunk createKMemoryChunk() {
+        return new HeapMemoryChunk();
     }
 }
