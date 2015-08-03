9fc686630336c981514d963936a127afb0eea2b1
Francois Fouquet <fouquet.f@gmail.com>
2015-08-03 18:17:59 +0200

    fix

--- org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/map/impl/ArrayLongLongMapTest.java
+++ org.kevoree.modeling.microframework/src/test/java/org/kevoree/modeling/memory/map/impl/ArrayLongLongMapTest.java
@@ -1,6 +1,5 @@
 package org.kevoree.modeling.memory.map.impl;
 
-import org.kevoree.modeling.KConfig;
 import org.kevoree.modeling.memory.chunk.impl.ArrayLongLongMap;
 import org.kevoree.modeling.memory.map.BaseKLongLongMapTest;
 import org.kevoree.modeling.memory.chunk.KLongLongMap;
@@ -9,7 +8,7 @@ public class ArrayLongLongMapTest extends BaseKLongLongMapTest {
 
     @Override
     public KLongLongMap createKLongLongMap() {
-        return new ArrayLongLongMap(KConfig.CACHE_INIT_SIZE, KConfig.CACHE_LOAD_FACTOR);
+        return new ArrayLongLongMap(null);
     }
 
 }
