f6620e352b956add0b77d44de5516e7e4099e0b2
Francois Fouquet <fouquet.f@gmail.com>
2015-08-05 12:08:05 +0200

    add native array 2D

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/util/maths/structure/impl/NativeArray2D.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/util/maths/structure/impl/NativeArray2D.java
@@ -1,7 +1,5 @@
 package org.kevoree.modeling.util.maths.structure.impl;
 
-import org.kevoree.modeling.memory.chunk.KObjectChunk;
-import org.kevoree.modeling.meta.KMetaClass;
 import org.kevoree.modeling.util.maths.structure.KArray2D;
 
 public class NativeArray2D implements KArray2D {
@@ -12,19 +10,13 @@ public class NativeArray2D implements KArray2D {
 
     private int _offset;
 
-    private int _segmentIndex;
+    private double[] _back;
 
-    private KObjectChunk _segment;
-
-    private KMetaClass _metaClass;
-
-    public NativeArray2D(int p_nbRows, int p_nbColumns, int p_offset, int p_segmentIndex, KObjectChunk p_segment, KMetaClass p_metaClass) {
+    public NativeArray2D(int p_nbRows, int p_nbColumns, int p_offset, double[] p_back) {
         this._nbRows = p_nbRows;
         this._nbColumns = p_nbColumns;
         this._offset = p_offset;
-        this._segment = p_segment;
-        this._segmentIndex = p_segmentIndex;
-        this._metaClass = p_metaClass;
+        this._back = p_back;
     }
 
     @Override
@@ -39,17 +31,17 @@ public class NativeArray2D implements KArray2D {
 
     @Override
     public double get(int p_rowIndex, int p_columnIndex) {
-        return this._segment.getDoubleArrayElem(this._segmentIndex, this._offset + (p_rowIndex * this._nbColumns) + p_columnIndex, this._metaClass);
+        return this._back[this._offset + (p_rowIndex * this._nbColumns) + p_columnIndex];
     }
 
     @Override
     public double set(int p_rowIndex, int p_columnIndex, double value) {
-        this._segment.setDoubleArrayElem(this._segmentIndex, this._offset + (p_rowIndex * this._nbColumns) + p_columnIndex, value, this._metaClass);
+        this._back[this._offset + (p_rowIndex * this._nbColumns) + p_columnIndex] = value;
         return value;
     }
 
     @Override
     public double add(int rawIndex, int columnIndex, double value) {
-        return set(rawIndex,columnIndex,get(rawIndex,columnIndex)+value);
+        return set(rawIndex, columnIndex, get(rawIndex, columnIndex) + value);
     }
 }
