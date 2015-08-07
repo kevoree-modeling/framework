233b1479442ae9aa96e3a500ab04d3ba05ed005c
Grégory Nain <gregory.nain@gmail.com>
2015-08-07 11:23:29 +0200

    call run on lookupPreciseKey

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/memory/manager/impl/DataManager.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/memory/manager/impl/DataManager.java
@@ -346,7 +346,7 @@ public class DataManager implements KDataManager, KInternalDataManager {
                             public void on(KObject[] updatedObjects) {
                                 _listenerManager.dispatch(updatedObjects);
                             }
-                        });
+                        }).run();
                     }
                 });
             }
