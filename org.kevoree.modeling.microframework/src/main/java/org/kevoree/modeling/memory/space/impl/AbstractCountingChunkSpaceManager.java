5a5cce5939f883c225dcd9c2d00195770275208e
Francois Fouquet <fouquet.f@gmail.com>
2015-08-10 10:47:10 +0200

    try to clean immeditalty

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/memory/space/impl/AbstractCountingChunkSpaceManager.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/memory/space/impl/AbstractCountingChunkSpaceManager.java
@@ -76,7 +76,10 @@ public abstract class AbstractCountingChunkSpaceManager implements KChunkSpaceMa
     public KObjectChunk cloneMarkAndUnmark(KObjectChunk previous, long newUniverse, long newTime, long obj, KMetaModel metaModel) {
         KObjectChunk newCreatedElement = _space.clone(previous, newUniverse, newTime, obj, metaModel);
         newCreatedElement.inc();
-        previous.dec();
+        int newCount = previous.dec();
+        if (newCount == 0 && (previous.getFlags() & KChunkFlags.DIRTY_BIT) != KChunkFlags.DIRTY_BIT) {
+            _space.remove(previous.universe(), previous.time(), previous.obj(), _metaModel);
+        }
         return newCreatedElement;
     }
 
