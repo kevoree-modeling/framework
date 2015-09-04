eab269e441399749e5f7e81f8847c620b263e23d
Assaad Moawad <assaad.moawad@uni.lu>
2015-09-04 11:44:49 +0200

    Javadoc for the Blas used

--- org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/util/maths/structure/blas/KBlas.java
+++ org.kevoree.modeling.microframework/src/main/java/org/kevoree/modeling/util/maths/structure/blas/KBlas.java
@@ -4,21 +4,78 @@ import org.kevoree.modeling.util.maths.structure.KArray2D;
 
 public interface KBlas {
 
-    //For Matrix Multiplications
-    void dgemm(KBlasTransposeType paramString1, KBlasTransposeType paramString2, int paramInt1, int paramInt2, int paramInt3, double paramDouble1, double[] paramArrayOfDouble1, int paramInt4, int paramInt5, double[] paramArrayOfDouble2, int paramInt6, int paramInt7, double paramDouble2, double[] paramArrayOfDouble3, int paramInt8, int paramInt9);
+    /**
+     * Matrix multiplications
+     * matC = alpha*matA*matB + beta*matC
+     * @param transA: if we need to transpose matrix A
+     * @param transB: if we need to transpose matrix B
+     * @param m: rows for C
+     * @param n: columns for C
+     * @param k: intermediate dimension
+     * @param alpha: multiplicative parameter for matA*matB
+     * @param matA: first matrix
+     * @param offsetA: offset in the array of matA, usually 0
+     * @param ldA: number of rows in matA to jump to reach the second column
+     * @param matB: second matrix
+     * @param offsetB: offset in the array of matB, usually 0
+     * @param ldB: number of rows in matB to jump to reach the second column
+     * @param beta: scaling parameter for matC
+     * @param matC: third matrix, which contains the result.
+     * @param offsetC: offset in the array of matC, usually 0
+     * @param ldC: number of rows in matC to jump to reach the second column     */
+    void dgemm(KBlasTransposeType transA, KBlasTransposeType transB, int m, int n, int k, double alpha, double[] matA, int offsetA, int ldA, double[] matB, int offsetB, int ldB, double beta, double[] matC, int offsetC, int ldC);
 
-    //To solve AX=B
-    void dgetrs(KBlasTransposeType paramString, int paramInt1, int paramInt2, double[] paramArrayOfDouble1, int paramInt3, int paramInt4, int[] paramArrayOfInt, int paramInt5, double[] paramArrayOfDouble2, int paramInt6, int paramInt7, int[] paramintW);
+    /**
+     * To solve AX=B
+     * @param transA: if we need to transpose matrix A
+     * @param dim: Dimension of matrix A
+     * @param nrhs: Number of column in matrix B
+     * @param matA: matrix A
+     * @param offsetA: offset in the array of matrix A
+     * @param ldA: number of rows in matA to jump to reach the second column
+     * @param ipiv: The pivot indices from DGETRF;
+     * @param offsetIpiv: Offset of IPIV
+     * @param matB: matrix B -> it will become the result at the end.
+     * @param offsetB: offset in the array of matrix B
+     * @param ldB: number of rows in matB to jump to reach the second column
+     * @param info: info[0]=0 -> successful exit
+     */
+    void dgetrs(KBlasTransposeType transA, int dim, int nrhs, double[] matA, int offsetA, int ldA, int[] ipiv, int offsetIpiv, double[] matB, int offsetB, int ldB, int[] info);
 
-    //To invert a matrix
-    void dgetri(int paramInt1, double[] paramArrayOfDouble1, int paramInt2, int paramInt3, int[] paramArrayOfInt, int paramInt4, double[] paramArrayOfDouble2, int paramInt5, int paramInt6, int[] paramintW);
+    /**
+     * To invert a matrix
+     * @param dim: Dimension of matrix A
+     * @param matA: MatrixA to invert -> the result will be stored here
+     * @param offsetA: offset in the array of matrix A
+     * @param ldA: number of rows in matA to jump to reach the second column
+     * @param ipiv: The pivot indices from DGETRF;
+     * @param offsetIpiv: Offset of IPIV
+     * @param work: A workspace for invert
+     * @param offsetWork: offset in the array of workspace
+     * @param ldWork: number of rows in workspace to jump to reach the second column
+     * @param info: info[0]=0 -> successful exit
+     */
+    void dgetri(int dim, double[] matA, int offsetA, int ldA, int[] ipiv, int offsetIpiv, double[] work, int offsetWork, int ldWork, int[] info);
 
-    //To factorize LU matrix
-    void dgetrf(int paramInt1, int paramInt2, double[] paramArrayOfDouble, int paramInt3, int paramInt4, int[] paramArrayOfInt, int paramInt5, int[] paramintW);
+    /**
+     * To factorize LU matrix
+     * @param rows: rows of matrix A
+     * @param columns: columns of matrix A
+     * @param matA: MatrixA to invert -> the result will be stored here
+     * @param offsetA: offset in the array of matrix A
+     * @param ldA: number of rows in matA to jump to reach the second column
+     * @param ipiv: The pivot indices from DGETRF;
+     * @param offsetIpiv: Offset of IPIV
+     * @param info: info[0]=0 -> successful exit
+     */
+    void dgetrf(int rows, int columns, double[] matA, int offsetA, int ldA, int[] ipiv, int offsetIpiv, int[] info);
 
-    //To transpose matrix
+    //To transpose matrix matA in result
     void trans(KArray2D matA, KArray2D result);
 
+    /**
+     * To shutdown the blas library
+     */
     void shutdown();
 
     //Level 1 Blas
