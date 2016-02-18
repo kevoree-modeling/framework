69a4c45b8686a332b002e2742529a068ded8331a
Thomas Hartmann <thomi.hartmann@gmail.com>
2016-02-18 16:31:05 +0100

    * off-heap implementations + fixes

---
+++ microframework/src/test/java/org/kevoree/modeling/memory/space/impl/PressOffHeapChunkSpaceTest.java
@@ -0,0 +1,39 @@
+package org.kevoree.modeling.memory.space.impl;
+
+import org.junit.Test;
+import org.kevoree.modeling.memory.KChunk;
+import org.kevoree.modeling.memory.KChunkFlags;
+import org.kevoree.modeling.memory.space.KChunkTypes;
+import org.kevoree.modeling.memory.space.impl.press.PressOffHeapChunkSpace;
+
+/** @ignore ts */
+public class PressOffHeapChunkSpaceTest {
+
+    @Test
+    public void monoThreadFullTest() {
+
+        PressOffHeapChunkSpace space = new PressOffHeapChunkSpace(10);
+        //Fill the entire cache
+        for (int i = 0; i < 10; i++) {
+            KChunk chunk = space.create(i, 0, 10, KChunkTypes.OBJECT_CHUNK,null);
+            chunk.setFlags(KChunkFlags.DIRTY_BIT, 0);
+        }
+        //space.create(1, 1, 1, KChunkTypes.OBJECT_CHUNK);
+        //space.create(2, 1, 1, KChunkTypes.OBJECT_CHUNK);
+        //space.create(3, 1, 1, KChunkTypes.OBJECT_CHUNK);
+        //space.create(4, 1, 1, KChunkTypes.OBJECT_CHUNK);
+
+        for (int i = 0; i < 3; i++) {
+            space.get(i, 0, 10).inc();
+        }
+
+        for (int i = 0; i < 8; i++) {
+            space.create(i, 1, 1, KChunkTypes.OBJECT_CHUNK,null);
+        }
+
+        System.out.println(space);
+
+
+    }
+
+}
